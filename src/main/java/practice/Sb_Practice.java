08f0eb31e7d44ad440d609621ce21745e317ecfa
Absimsek <aliberkaysimsekk@gmail.com>
2023-06-16 13:12:29 +0300

    StringBuilderPractice

---
+++ src/main/java/practice/Sb_Practice.java
@@ -0,0 +1,25 @@
+package practice;
+
+import java.util.Scanner;
+
+public class Sb_Practice {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("text");
+        StringBuilder sb=new StringBuilder(input.nextLine());
+int total=0;
+        String t=sb.toString().replaceAll("[^0-9]","");
+        System.out.println(t);
+        for (int i = 0; i <t.length() ; i++) {
+
+
+        }
+
+
+
+
+
+
+
+    }
+}
